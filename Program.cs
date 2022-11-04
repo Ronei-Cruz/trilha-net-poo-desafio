033043ec13e82ee2872532e664c0397cccb470a4
Ronei-Cruz <roneidiegocruz@gmail.com>
2022-11-04 08:57:36 -0300

    Completando desafio Pottencial, implementando atributos e metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarth Nokia");
+Smartphone nokia = new Nokia(numero: "1234567890", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarth iPhone");
+Smartphone iphone = new Iphone(numero: "9876543210", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
